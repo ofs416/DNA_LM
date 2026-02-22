ddc65ba0efc0707a2c8a142054488029cca95670
ofs416 <oskarfstone@gmail.com>
2026-02-22 00:37:47 +0000

    started on the transformer

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -2,8 +2,14 @@ import torch.nn as nn
 
 
 class Transformer(nn.Module):
-    def __init__(self, input_dim, model_dim, num_heads, num_layers, output_dim):
+    def __init__(
+        self, input_dim, model_dim, num_heads, num_layers, output_dim, embedder
+    ):
         super().__init__()
+        self.embedder = embedder
+        self.W_q = nn.Linear(model_dim, model_dim, bias=False)
+        self.W_k = nn.Linear(model_dim, model_dim, bias=False)
+        self.W_v = nn.Linear(model_dim, model_dim, bias=False)
 
-    def forward(self):
+    def forward(self, input):
         return
