Fixed masking and correcting head dim in attention

src/models/attention.py
@@ -8,27 +8,30 @@ class MHA(nn.Module):
         super().__init__()
         self.model_dim = model_dim
         self.num_heads = num_heads
-        self.W_q = nn.Linear(self.model_dim, self.model_dim * num_heads, bias=False)
-        self.W_k = nn.Linear(self.model_dim, self.model_dim * num_heads, bias=False)
-        self.W_v = nn.Linear(self.model_dim, self.model_dim * num_heads, bias=False)
+        self.head_dim = model_dim // num_heads
+
+        self.W_q = nn.Linear(self.model_dim, self.model_dim, bias=False)
+        self.W_k = nn.Linear(self.model_dim, self.model_dim, bias=False)
+        self.W_v = nn.Linear(self.model_dim, self.model_dim, bias=False)
 
     def forward(self, query, key, value, mask=None):
         # Input shape: (batch_size, seq_len, model_dim)
         batch_size = query.size(0)
 
         Q, K, V = self.W_q(query), self.W_k(key), self.W_v(value)
-        Q = Q.view(batch_size, -1, self.num_heads, self.model_dim).transpose(1, 2)
-        K = K.view(batch_size, -1, self.num_heads, self.model_dim).transpose(1, 2)
-        V = V.view(batch_size, -1, self.num_heads, self.model_dim).transpose(1, 2)
+        Q = Q.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        K = K.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        V = V.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
 
-        attention_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.model_dim**0.5)
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1)) / (self.head_dim**0.5)
 
         if mask is not None:
+            mask = mask.unsqueeze(1).unsqueeze(2)
             attention_scores = attention_scores.masked_fill(mask == 0, float("-inf"))
 
         attention_weights = F.softmax(attention_scores, dim=-1)
         Z = torch.matmul(attention_weights, V)
-        # Z shape: (batch_size, num_heads, seq_len, model_dim)
+        Z = Z.transpose(1, 2).contiguous().view(batch_size, -1, self.model_dim)
 
         return Z, attention_weights
 

src/models/transformer.py
@@ -14,25 +14,19 @@ class Transformer(nn.Module):
         self.embedder = nn.Embedding(num_unique_tokens, model_dim)
         self.pos_encoder = AbsPosEncoder(max_len, model_dim)
         self.mha = MHA(model_dim, num_heads)
-        self.dense = nn.Linear(model_dim * num_heads, model_dim)
+        self.dense = nn.Linear(model_dim, model_dim)
         self.norm = nn.LayerNorm(model_dim)
 
-    def forward(self, tokens):
+    def forward(self, tokens, mask=None):
         embeddings = self.embedder(tokens)
         pos_embeddings = self.pos_encoder(embeddings)
         embeddings_norm = self.norm(pos_embeddings)
         mha_output_norm, attention_weights = self.mha(
-            embeddings_norm, embeddings_norm, embeddings_norm
+            embeddings_norm, embeddings_norm, embeddings_norm, mask=mask
         )
         resdiual_output = (mha_output_norm + embeddings.unsqueeze(1)).transpose(1, 2)
         # resdiual_output shape: (batch_size, seq_len, num_heads, model_dim)
-        dense_output = self.dense(
-            resdiual_output.reshape(
-                resdiual_output.size(0),
-                resdiual_output.size(1),
-                resdiual_output.size(2) * resdiual_output.size(3),
-            )
-        )
+        dense_output = self.dense(resdiual_output)
 
         return dense_output
 