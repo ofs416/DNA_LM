856d044c7b59c775c6b24b43a425ddd19d62d58e
ofs416 <oskarfstone@gmail.com>
2026-03-08 22:23:57 +0000

    removed embedding from transformer layer and fixed cuda detection issue

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -18,13 +18,12 @@ class Transformer(nn.Module):
         self.norm = nn.LayerNorm(model_dim)
 
     def forward(self, tokens, mask=None):
-        embeddings = self.embedder(tokens)
-        pos_embeddings = self.pos_encoder(embeddings)
+        pos_embeddings = self.pos_encoder(tokens)
         embeddings_norm = self.norm(pos_embeddings)
         mha_output_norm, attention_weights = self.mha(
             embeddings_norm, embeddings_norm, embeddings_norm, mask=mask
         )
-        resdiual_output = (mha_output_norm + embeddings.unsqueeze(1)).transpose(1, 2)
+        resdiual_output = mha_output_norm + pos_embeddings
         # resdiual_output shape: (batch_size, seq_len, num_heads, model_dim)
         dense_output = self.dense(resdiual_output)
 
@@ -40,18 +39,13 @@ if __name__ == "__main__":
 
     embedder = nn.Embedding(unique_tokens, model_dim)
 
-    input1 = torch.randint(0, unique_tokens, (batch_size, seq_len))  # Random token IDs
-    input2 = torch.rand(batch_size, seq_len, model_dim)
-    print(
-        f"Input1 shape: {embedder(input1).shape}"
-    )  # Expected shape: (batch_size, seq_len, model_dim)
-    print(
-        f"Input2 shape: {input2.shape}"
-    )  # Expected shape: (batch_size, seq_len, model_dim)
+    inputs = torch.randint(0, unique_tokens, (batch_size, seq_len))  # Random token IDs
+
+    embedded_inputs = embedder(inputs)  # Shape: (batch_size, seq_len, model_dim)
 
     transformer = Transformer(unique_tokens, model_dim, num_heads, max_len=100)
 
-    transformer_output = transformer(input1)
+    transformer_output = transformer(embedded_inputs)
     print(
         f"Transformer output shape: {transformer_output.shape}"
     )  # Expected shape: (batch_size, seq_len, model_dim)
